0d84f884a65cdf12635adab7b1ba063f10630407
LastEternity <adrian.s.rosing@gmail.com>
2024-05-01 18:10:22 -0700

    Reorganized webapp.py a little.

--- webapp.py
+++ webapp.py
@@ -3,6 +3,12 @@ from fastapi.responses import HTMLResponse    # Used for returning HTML response
 from fastapi.staticfiles import StaticFiles   # Used for serving static files
 import uvicorn                                # Used for running the app
 
+
+
+##########################################
+#            Global Variables            #
+##########################################
+
 # Configuration
 app = FastAPI()         
 
@@ -13,6 +19,12 @@ sample_data = [0, "Pothole", loc_sample, .35, None] #Not sure what the readings 
 
 # Mount the static directory
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+
+##########################################
+#                 Routes                 #
+##########################################
 
 # Return home page
 @app.get("/", response_class=HTMLResponse)
@@ -26,26 +38,28 @@ def get_html() -> HTMLResponse:
   with open("dashboard.html") as html:
     return HTMLResponse(content=html.read())
   
-####  We can worry about these later:
-# Route To Log User Out
-@app.post("/api/auth/logout")
-def logout_user():
-    #TODO: Implement
-    return {"Logged out; not yet implemented."}
-
-#Route to Log User In:
-@app.post("/api/auth/login")
-def login_user():
-    #TODO: Implement
-    return {"Logged in; not yet implemented."}
-
-#Create User:
-@app.put("/api/auth/register")
-def register_user():
-   #TODO: Implement
-   return {"Created account; not yet implemented."}
 
 
+##########################################
+#            Helper Functions            #
+##########################################
+def potholeDetected(loc, severity, readings):
+   return
+
+### IF WE LIKE THIS STRUCTURE THEN WE MAY NEED TO MAKE A FUNCTION FOR EVERY OTHER
+### INCIDENT TYPE.
+
+# Calls Respective Incident Type In Case We Need It:
+def type_of_incident(incident, loc, severity, readings):
+   if(incident == "Pothole"):
+      potholeDetected(loc, severity, readings)
+
+
+
+
+##########################################
+#              API Functions             #
+##########################################
 # Get All Incidents From Database   ###NOTE WE MAY WANT TO MAKE IT ONLY GET LOCAL INCIDENTS.
 @app.get("/api/incidents")
 def get_incidents():
@@ -74,20 +88,42 @@ def get_dashboard_data():
     # Will likely need to call incidents and update graph?
     return {"Not yet implemented"}
 
-
-#Individual Function For Defining Behavior
-def potholeDetected(loc, severity, readings):
-   return
-
-### IF WE LIKE THIS STRUCTURE THEN WE MAY NEED TO MAKE A FUNCTION FOR EVERY OTHER
-### INCIDENT TYPE.
-
-# Calls Respective Incident Type In Case We Need It:
-def type_of_incident(incident, loc, severity, readings):
-   if(incident == "Pothole"):
-      potholeDetected(loc, severity, readings)
    
 
+##########################################
+#             Authentication             #
+##########################################
+# Route To Log User Out
+@app.post("/api/auth/logout")
+def logout_user():
+    #TODO: Implement
+    return {"Logged out; not yet implemented."}
+
+#Route to Log User In:
+@app.post("/api/auth/login")
+def login_user():
+    #TODO: Implement
+    return {"Logged in; not yet implemented."}
+
+#Create User:
+@app.put("/api/auth/register")
+def register_user():
+   #TODO: Implement
+   return {"Created account; not yet implemented."}
+
+
+
+##########################################
+#                  Main                  #
+##########################################
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=6543)
+
+
+
+##########################################
+#                 Notes                  #
+##########################################
 # Steps
 # Website Must Be Able To:
 # 1) Receives Data From Phone: location, severity, type of incident, user confirmation, ultrasound readings, accelerometer readings, gyroscope readings.
@@ -106,6 +142,3 @@ def type_of_incident(incident, loc, severity, readings):
 ########  2  ##########
 # Create Login Routes (Optional)
 # Create Basic Text Display of Data on Dashboard  --->  Ultimately Will Tie to Google Plugin
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=6543)
